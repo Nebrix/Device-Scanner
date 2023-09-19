26ec5ae5f3c871f891188053dc527a6a4d98599d
Varkely <109563566+varkely@users.noreply.github.com>
2023-09-20 00:03:45 +0100

    added lowercase for selections

--- etc/buildexe.py
+++ etc/buildexe.py
@@ -1,21 +1,26 @@
 import os
 
+WINDOWS = "Windows"
+LINUX = "Linux"
+CLI = "CLI"
+GUI = "GUI"
+
 print("== Build Exe ==")
 
 print("Windows or Linux")
 userinput = input("Enter what OS to build for: ")
 
-if userinput == "Windows":
+if userinput == WINDOWS or userinput == WINDOWS.lower():
     print("CLI or GUI")
     selectExe = input("Enter the Exe you want to build: ")
-    if selectExe == "CLI":
+    if selectExe == CLI or selectExe == CLI.lower():
         os.system("powershell.exe -File .\\build\\build-cli\\windows\\windowscli.ps1")
-    elif selectExe == "GUI":
+    elif selectExe == GUI or selectExe == GUI.lower():
         os.system("powershell.exe -File .\\build\\build-gui\\windows\\windowsgui.ps1")
-elif userinput == "Linux":
+elif userinput == LINUX or userinput == LINUX.lower():
     print("CLI or GUI")
     selectExe = input("Enter the Exe you want to build: ")
-    if selectExe == "CLI":
+    if selectExe == CLI or selectExe == CLI.lower():
         os.system("./build/build-cli/linux/linuxcli.sh")
-    elif selectExe == "GUI":
+    elif selectExe == GUI or selectExe == GUI.lower():
         os.system("./build/build-gui/linux/linuxgui.sh")
