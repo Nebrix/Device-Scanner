0aa6971a1337b08394cf2ec52a7c39d154968bf5
Codezz-ops <Codezz-ops@protonmail.com>
2023-09-19 21:49:49 -0400

    Refactored buildexe code

--- etc/buildexe.py
+++ etc/buildexe.py
@@ -1,21 +1,38 @@
 import os
 
+WINDOWS = "Windows"
+LINUX = "Linux"
+CLI = "CLI"
+GUI = "GUI"
+
+def build_windows(selected_exe):
+    if selected_exe == CLI or selected_exe == CLI.lower():
+        os.system("powershell.exe -File .\\build\\build-cli\\windows\\windowscli.ps1")
+    elif selected_exe == GUI or selected_exe == GUI.lower():
+        os.system("powershell.exe -File .\\build\\build-gui\\windows\\windowsgui.ps1")
+    else:
+        print("Invalid selection. Exiting...")
+
+def build_linux(selected_exe):
+    if selected_exe == CLI or selected_exe == CLI.lower():
+        os.system("./build/build-cli/linux/linuxcli.sh")
+    elif selected_exe == GUI or selected_exe == GUI.lower():
+        os.system("./build/build-gui/linux/linuxgui.sh")
+    else:
+        print("Invalid selection. Exiting...")
+
 print("== Build Exe ==")
 
 print("Windows or Linux")
-userinput = input("Enter what OS to build for: ")
+user_input = input("Enter what OS to build for: ")
 
-if userinput == "Windows":
+if user_input.lower() == WINDOWS.lower():
     print("CLI or GUI")
-    selectExe = input("Enter the Exe you want to build: ")
-    if selectExe == "CLI":
-        os.system("powershell.exe -File .\\build\\build-cli\\windows\\windowscli.ps1")
-    elif selectExe == "GUI":
-        os.system("powershell.exe -File .\\build\\build-gui\\windows\\windowsgui.ps1")
-elif userinput == "Linux":
+    select_exe = input("Enter the Exe you want to build: ")
+    build_windows(select_exe)
+elif user_input.lower() == LINUX.lower():
     print("CLI or GUI")
-    selectExe = input("Enter the Exe you want to build: ")
-    if selectExe == "CLI":
-        os.system("./build/build-cli/linux/linuxcli.sh")
-    elif selectExe == "GUI":
-        os.system("./build/build-gui/linux/linuxgui.sh")
+    select_exe = input("Enter the Exe you want to build: ")
+    build_linux(select_exe)
+else:
+    print("Invalid OS selection. Exiting...")
